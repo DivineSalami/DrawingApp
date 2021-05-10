68c5fd7f2dbe58748516a18bf6806ca2b618c76e
p.m.student <Bolden.Chance02@gmail.com>
2021-05-10 12:37:20 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by P.M. Student on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
